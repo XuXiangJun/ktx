7c6753b592b64f2babbd343a619c47a90d16e004
XuXiangJun <xuxiangjun@live.cn>
2021-08-19 14:25:10 +0800

    update

--- src/main/kotlin/com/github/xuxiangjun/javaext/IOExt.kt
+++ src/main/kotlin/com/github/xuxiangjun/javaext/IOExt.kt
@@ -22,25 +22,48 @@ fun InputStream.readUInt8(): Int {
     return read() and 0xff
 }
 
-fun InputStream.readUInt16(): Int {
-    return (read() and 0xff) or
-            ((read() and 0xff) shl 8)
+fun InputStream.readUInt16(littleEndian: Boolean = true): Int {
+    return if (littleEndian) {
+        (read() and 0xff) or
+                ((read() and 0xff) shl 8)
+    } else {
+        ((read() and 0xff) shl 8) or
+                (read() and 0xff)
+    }
 }
 
-fun InputStream.readUInt32(): Long {
-    return (read().toLong() and 0xffL) or
-            ((read().toLong() and 0xffL) shl 8) or
-            ((read().toLong() and 0xffL) shl 16) or
-            ((read().toLong() and 0xffL) shl 24)
+fun InputStream.readUInt32(littleEndian: Boolean = true): Long {
+    return if (littleEndian) {
+        (read().toLong() and 0xffL) or
+                ((read().toLong() and 0xffL) shl 8) or
+                ((read().toLong() and 0xffL) shl 16) or
+                ((read().toLong() and 0xffL) shl 24)
+    } else {
+        ((read().toLong() and 0xffL) shl 24) or
+                ((read().toLong() and 0xffL) shl 16) or
+                ((read().toLong() and 0xffL) shl 8) or
+                (read().toLong() and 0xffL)
+    }
 }
 
-fun InputStream.readInt64(): Long {
-    return (read().toLong() and 0xffL) or
-            ((read().toLong() and 0xffL) shl 8) or
-            ((read().toLong() and 0xffL) shl 16) or
-            ((read().toLong() and 0xffL) shl 24) or
-            ((read().toLong() and 0xffL) shl 32) or
-            ((read().toLong() and 0xffL) shl 40) or
-            ((read().toLong() and 0xffL) shl 48) or
-            ((read().toLong() and 0xffL) shl 56)
+fun InputStream.readInt64(littleEndian: Boolean = true): Long {
+    return if (littleEndian) {
+        (read().toLong() and 0xffL) or
+                ((read().toLong() and 0xffL) shl 8) or
+                ((read().toLong() and 0xffL) shl 16) or
+                ((read().toLong() and 0xffL) shl 24) or
+                ((read().toLong() and 0xffL) shl 32) or
+                ((read().toLong() and 0xffL) shl 40) or
+                ((read().toLong() and 0xffL) shl 48) or
+                ((read().toLong() and 0xffL) shl 56)
+    } else {
+        ((read().toLong() and 0xffL) shl 56) or
+                ((read().toLong() and 0xffL) shl 48) or
+                ((read().toLong() and 0xffL) shl 40) or
+                ((read().toLong() and 0xffL) shl 32) or
+                ((read().toLong() and 0xffL) shl 24) or
+                ((read().toLong() and 0xffL) shl 16) or
+                ((read().toLong() and 0xffL) shl 8) or
+                (read().toLong() and 0xffL)
+    }
 }
