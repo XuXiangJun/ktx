7fc2f36bd255b422e596ab658d69023912a6f6d7
XuXiangJun <xuxiangjun@live.cn>
2021-08-26 18:57:50 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     kotlin("jvm") version "1.5.30"
-//    id("com.github.dcendents.android-maven") version "2.1"
+    id("com.github.dcendents.android-maven") version "2.1"
 }
 
 group = "com.github.xuxiangjun"
-version = "0.5"
+version = "0.5.1"
 
 repositories {
     mavenCentral()
