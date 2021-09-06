ca038aaaad03c028fbca07fb1302ce1d37b3b4a3
XuXiangJun <xuxiangjun@live.cn>
2021-09-06 10:37:10 +0800

    update

--- src/main/kotlin/com/github/xuxiangjun/javaext/CharSequenceExt.kt
+++ src/main/kotlin/com/github/xuxiangjun/javaext/CharSequenceExt.kt
@@ -50,4 +50,10 @@ fun CharSequence.sha256(srcCharset: Charset = Charsets.UTF_8): ByteArray {
     return md.digest(toString().toByteArray(srcCharset))
 }
 
+fun CharSequence.isEmail(): Boolean {
+    val regex =
+        Regex("^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))\$")
+    return regex.matches(this)
+}
+
 
