5cb3f88855935a712de08a95c290e67f50f9e547
XuXiangJun <xuxiangjun@live.cn>
2021-10-27 11:46:23 +0800

    update

---
+++ src/main/kotlin/com/github/xuxiangjun/javaext/RandomExt.kt
@@ -0,0 +1,21 @@
+package com.github.xuxiangjun.javaext
+
+import kotlin.random.Random
+
+fun randomInteger(length: Int): String {
+    val builder = StringBuilder()
+    for (i in 0 until length) {
+        builder.append(Random.nextInt(10))
+    }
+    return builder.toString()
+}
+
+fun randomString(length: Int): String {
+    val builder = StringBuilder()
+    for (i in 0 until length) {
+        val r = Random.nextInt(52)
+        val base = if (r < 26) 'a'.code else 'A'.code - 26
+        builder.append((base + r).toChar())
+    }
+    return builder.toString()
+}
