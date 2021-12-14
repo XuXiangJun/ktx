2298cadcdf30d45846c1288c786ee0547fcd94e8
XuXiangJun <xuxiangjun@live.cn>
2021-12-14 12:31:29 +0800

    update

---
+++ src/main/kotlin/com/github/xuxiangjun/javaext/LocaleExt.kt
@@ -0,0 +1,94 @@
+package com.github.xuxiangjun.javaext
+
+enum class LanguageX(code: String) {
+    Arabic("ar"), // 阿拉伯语
+    Bulgarian("bg"), // 保加利亚语
+    Catalan("ca"), // 加泰罗尼亚语
+    Czech("cs"), // 捷克语
+    Danish("da"), // 丹麦语
+    German("de"), // 德语
+    Greek("el"), // 希腊语
+    English("en"), // 英语
+    Spanish("es"), // 西班牙语
+    Finnish("fi"), // 芬兰语
+    French("fr"), // 法语
+    Hebrew("he"), // 希伯来语
+    Hindi("hi"), // 印地语
+    Croatian("hr"), // 克罗地亚语
+    Hungarian("hu"), // 匈牙利语
+    Indonesian("id"), // 印度尼西亚语
+    Italian("it"), // 意大利语
+    Japanese("ja"), // 日语
+    Lithuanian("lt"), // 立陶宛语
+    Latvian("lv"), // 拉脱维亚语
+    NorwegianBokmol("nb"), // 挪威语
+    Dutch("nl"), // 荷兰语
+    Polish("pl"), // 波兰语
+    Portuguese("pt"), // 葡萄牙语
+    Romanian("ro"), // 罗马尼亚语
+    Russian("ru"), // 俄语
+    Slovak("sk"), // 斯洛伐克语
+    Slovenian("sl"), // 斯洛文尼亚语
+    Serbian("sr"), // 塞尔维亚语
+    Swedish("sv"), // 瑞典语
+    Thai("th"), // 泰语
+    Tagalog("tl"), // 他加禄语
+    Turkish("tr"), // 土耳其语
+    Ukrainian("uk"), // 乌克兰语
+    Vietnamese("vi"), // 越南语
+    Chinese("zh"), // 中文
+    ;
+}
+
+enum class LocaleX(code: String) {
+    Australia("AU"), // 澳大利亚
+    Austria("AT"), // 奥地利
+    Belgium("BE"), // 比利时
+    Brazil("BR"), // 巴西
+    Britain("GB"), // 英国
+    Bulgaria("BG"), // 保加利亚
+    Canada("CA"), // 加拿大
+    China("CN"), // 中国
+    Croatia("HR"), // 克罗地亚
+    CzechRepublic("CZ"), // 捷克
+    Denmark("DK"), // 丹麦
+    Finland("FI"), // 芬兰
+    France("FR"), // 法国
+    Germany("DE"), // 德国
+    Greece("GR"), // 希腊
+    Egypt("EG"), // 埃及
+    Hungary("HU"), // 匈牙利
+    India("IN"), // 印度
+    Indonesia("ID"), // 印度尼西亚
+    Ireland("IE"), // 爱尔兰
+    Israel("IL"), // 以色列
+    Italy("IT"), // 意大利
+    Japan("JP"), // 日本
+    Latvia("LV"), // 拉脱维亚
+    Liechtenstein("LI"), // 列支敦士登
+    Lithuania("LT"), // 立陶宛
+    Netherlands("NL"), // 荷兰
+    NewZealand("NZ"), // 新西兰
+    Norway("NO"), // 挪威
+    Philippines("PH"), // 菲律宾
+    Poland("PL"), // 波兰
+    Portugal("PT"), // 葡萄牙
+    Romania("RO"), // 罗马尼亚
+    Russia("RU"), // 俄罗斯
+    Serbia("RS"), // 塞尔维亚
+    Singapore("SG"), // 新加坡
+    Slovakia("SK"), // 斯洛伐克
+    Slovenia("SI"), // 斯洛文尼亚
+    Spain("ES"), // 西班牙
+    Sweden("SE"), // 瑞典
+    Switzerland("CH"), // 瑞士
+    Taiwan("TW"), // 中国台湾
+    Thailand("TH"), // 泰国
+    Turkey("TR"), // 土耳其
+    Ukraine("UA"), // 乌克兰
+    US("US"), // 美国
+    Vietnam("VN"), // 越南
+    Zimbabwe("ZA"), // 津巴布韦
+    ;
+}
+
