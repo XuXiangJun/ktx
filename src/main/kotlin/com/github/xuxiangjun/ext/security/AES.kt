417c0c11af49979033cdcdb9ed8e714d4c0fd862
XuXiangJun <xuxiangjun@live.cn>
2022-09-21 15:25:05 +0800

    Add AES-GCM

--- src/main/kotlin/com/github/xuxiangjun/ext/security/AES.kt
+++ src/main/kotlin/com/github/xuxiangjun/ext/security/AES.kt
@@ -1,39 +1,67 @@
 package com.github.xuxiangjun.ext.security
 
+import java.security.spec.AlgorithmParameterSpec
 import javax.crypto.Cipher
+import javax.crypto.spec.GCMParameterSpec
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
 object AES {
+    enum class Type {
+        ECB,
+        CBC,
+        CFB,
+        OFB,
+        GCM
+    }
+
     fun ecb(key: ByteArray, padding: Padding = Padding.PKCS5Padding): AESCipher {
-        return AESCipher(key, "AES/ECB/${padding.name}")
+        return AESCipher(Type.ECB, padding, key)
     }
 
     fun cbc(key: ByteArray, iv: ByteArray, padding: Padding = Padding.PKCS5Padding): AESCipher {
-        return AESCipher(key, "AES/CBC/${padding.name}", iv)
+        return AESCipher(Type.CBC, padding, key, iv)
     }
 
     fun cfb(key: ByteArray, iv: ByteArray, padding: Padding = Padding.PKCS5Padding): AESCipher {
-        return AESCipher(key, "AES/CFB/${padding.name}", iv)
+        return AESCipher(Type.CFB, padding, key, iv)
     }
 
     fun ofb(key: ByteArray, iv: ByteArray, padding: Padding = Padding.PKCS5Padding): AESCipher {
-        return AESCipher(key, "AES/OFB/${padding.name}", iv)
+        return AESCipher(Type.OFB, padding, key, iv)
+    }
+
+    fun gcm(key: ByteArray, iv: ByteArray, padding: Padding = Padding.PKCS5Padding): AESCipher {
+        return AESCipher(Type.GCM, padding, key, iv)
     }
 
     class AESCipher(
+        private val type: Type,
+        private val padding: Padding,
         private val key: ByteArray,
-        private val transformation: String,
         private val iv: ByteArray? = null
     ) {
+        private val transformation: String = "AES/${type.name}/${padding.name}"
+
+        private fun getParams(): AlgorithmParameterSpec? {
+            return when (type) {
+                Type.ECB -> null
+                Type.CBC -> IvParameterSpec(iv, 0, 16)
+                Type.CFB -> IvParameterSpec(iv, 0, 16)
+                Type.OFB -> IvParameterSpec(iv, 0, 16)
+                Type.GCM -> GCMParameterSpec(128, iv, 0, 12)
+            }
+        }
+
         fun encrypt(data: ByteArray): ByteArray {
             val cipher = Cipher.getInstance(transformation)
             val secretKey = SecretKeySpec(key, "AES")
-            if (iv == null) {
+
+            val params = getParams()
+            if (params == null) {
                 cipher.init(Cipher.ENCRYPT_MODE, secretKey)
             } else {
-                val ivParam = IvParameterSpec(iv)
-                cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivParam)
+                cipher.init(Cipher.ENCRYPT_MODE, secretKey, params)
             }
 
             return cipher.doFinal(data)
@@ -42,11 +70,11 @@ object AES {
         fun decrypt(data: ByteArray): ByteArray {
             val cipher = Cipher.getInstance(transformation)
             val secretKey = SecretKeySpec(key, "AES")
-            if (iv == null) {
+            val params = getParams()
+            if (params == null) {
                 cipher.init(Cipher.DECRYPT_MODE, secretKey)
             } else {
-                val ivParam = IvParameterSpec(iv)
-                cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParam)
+                cipher.init(Cipher.DECRYPT_MODE, secretKey, params)
             }
 
             return cipher.doFinal(data)
